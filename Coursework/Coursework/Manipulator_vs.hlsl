5adc1b28c38b6b7f0eee7ef011d8b30dca923160
Camwig <2001551@uad.ac.uk>
2024-03-28 10:18:14 +0000

    Improved noise
    Now I need to setup some lighting to apply it to the shape. Ill just take the previous lighting shader. Then its fractional brownain motion

--- Coursework/Coursework/Manipulator_vs.hlsl
+++ Coursework/Coursework/Manipulator_vs.hlsl
@@ -31,28 +31,9 @@ OutputType main(InputType input)
 {
     OutputType output;
     
-    //    //output.position = input.position;
-    //input.position.y += getHeight(input.tex, texture0, sampler0, height);
-    
-    /*float getHeight(float2 _uv, Texture2D texture0, SamplerState sampler0, float offset)
-{
-    //Sample the height map texture and then multiplies it by the offset value
-    //Which then returns the height value
-    float3 height = texture0.SampleLevel(sampler0, _uv, 0).x;
-    return height.r * offset;
-}*/
-    
-    //float3 height = texture0.SampleLevel(sampler0, input.tex, 0).x;
-    //input.position.xyz += height * 1.f;
-
-	// Calculate the position of the vertex against the world, view, and projection matrices.
-    output.position = mul(input.position, worldMatrix);
-    output.position = mul(output.position, viewMatrix);
-    output.position = mul(output.position, projectionMatrix);
-    
     float n = 0.0f;
     float multiple = 0.25f;
-    float3 Input = float3(input.normal.x * multiple, input.normal.y * multiple, input.normal.z * multiple);
+    float3 Input = float3(input.position.x * multiple, input.position.y * multiple, input.position.z * multiple);
     n = color(Input);
     
     //if(n<= -1.0f)
@@ -63,7 +44,21 @@ OutputType main(InputType input)
     
     //float3 n = texture0.SampleLevel(sampler0, input.tex, 0).x;
     
-    output.position.xyz += (n * input.normal)*0.5f;
+    input.position.xyz += (n * input.normal)*0.5f;
+    
+    	// Calculate the position of the vertex against the world, view, and projection matrices.
+    output.position = mul(input.position, worldMatrix);
+    output.position = mul(output.position, viewMatrix);
+    output.position = mul(output.position, projectionMatrix);
+
+	// Store the texture coordinates for the pixel shader.
+    output.tex = input.tex;
+
+	// Calculate the normal vector against the world matrix only and normalise.
+    output.normal = mul(input.normal, (float3x3) worldMatrix);
+    output.normal = normalize(output.normal);
+
+    return output;
 
 	// Store the texture coordinates for the pixel shader.
     output.tex = input.tex;
